0d11487973f8f1ebf8086db44b306a06e0856eea
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-10-02 20:00:09 -0300

    MeuPedido e MeuPedidoDAO

---
+++ src/main/java/com/loja/loja/persistencia/MeuPedido.java
@@ -0,0 +1,25 @@
+package com.loja.loja.persistencia;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityScan
+@Builder
+public class MeuPedido implements Serializable {
+
+    @Id @GeneratedValue
+    private Long id;
+    private int qtde;
+
+    @ManyToOne
+    private Produto produto;
+}
---
+++ src/main/java/com/loja/loja/persistencia/MeuPedidoDAO.java
@@ -0,0 +1,3 @@
+package com.loja.loja.persistencia;
+
+public interface MeuPedidoDAO extends JPARepository<MeuPedido, Long> { }
